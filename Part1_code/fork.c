ff297868e40f5f921a2743c40758997eede214f5
Simon <muckot@gmail.com>
2024-11-06 18:25:25 +0100

    Did fork.c
    Co-authored-by: Abod <aabb00dd@users.noreply.github.com>
    Co-authored-by: Remasproblemas <Remasproblemas@users.noreply.github.com>

--- Part1_code/fork.c
+++ Part1_code/fork.c
@@ -4,23 +4,41 @@
 int main(int argc, char **argv)
 {
     pid_t pid;
+    pid_t pid2;
     unsigned i;
-    unsigned niterations = 100;
+    unsigned niterations = 1;
     pid = fork();
     if (pid == 0) {
-        for (i = 0; i < niterations; ++i)
+        // Is the first love child. UwU
+        for (i = 0; i < niterations; ++i) {
             printf("A = %d, ", i);
-            printf("In child => Own pid : %d\n", getpid());
-            printf("In child => Parent's pid : %d\n", pid);
-     } else {
-        for (i = 0; i < niterations; ++i)
-            printf("B = %d, ", i);
-            printf("In child => Own pid : %d\n", getpid());
-            printf("In child => Parent's pid : %d\n", pid);
+            printf("\n");
+        }
+        printf("In child (A) => Own pid : %d\n", getpid());
+        // printf("In child => Parent's pid : %d\n", pid);
+    } 
+    else 
+    {   
+        pid2 = fork();
+        if (pid2 == 0) {
+            // Catches the child from the second fork
+            for (i = 0; i < niterations; ++i) {
+                    printf("B = %d, ", i);
+                    printf("\n");
+                }
+                printf("In child (B) => Own pid : %d\n", getpid());
+                // printf("In child => Parent's pid : %d\n", pid2);
+        }
+        else {
+            // Catches original parent process
+            for (i = 0; i < niterations; ++i) {
+                    printf("C = %d, ", i);
+                    printf("\n");
+                }
+                printf("\n");
+                printf("In Parent (C) => Own pid : %d\n", getpid());
+                printf("In Parent (C) => First Child pid is %d\n", pid);
+                printf("In Parent (C) => Second Child pid is %d\n", pid2);
+            }
     }
-    printf("\n");
-    
-    // }
-    // else{
-    //   printf("In Parent => Child's pid is %d\n", pid);
 }
