29e0ccee022c98fdfb14bb2ca55bd4147e80ddab
Simon <muckot@gmail.com>
2024-11-13 18:38:44 +0100

    add new file for paralell version of matmul

--- Part1_code/matmul_paralell.c
+++ Part1_code/matmul_paralell.c
@@ -1,9 +1,19 @@
 /***************************************************************************
  *
- * Sequential version of Matrix-Matrix multiplication time: 4.81s
+ * Paralell version of matrix-matrix multiplication time:
  *
  ***************************************************************************/
-
+/*
+We will now parallelize the matrix multiplication program using threads. Parallelize the program according to the following
+assumptions:
+• Parallelize only the matrix multiplication, but not the initialization.
+• A new thread shall be created for each row to be calculated in the matrix.
+• The row number of the row that a thread shall calculate, shall be passed to the new thread as a parameter to the new
+thread.
+• The main function shall wait until all threads have terminated before the program terminates.
+Compile and link your parallel version of the matrix multiplication program. Don’t forget to add -lpthread when you
+link your program. We will now measure how much faster the parallel program is as compared to the sequential one.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
