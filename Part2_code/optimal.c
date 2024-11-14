cc401e8094ea77cc4bfe99241b56c43486177171
Simon <muckot@gmail.com>
2024-11-14 17:28:38 +0100

    Idk something changed

--- Part2_code/optimal.c
+++ Part2_code/optimal.c
@@ -17,38 +17,59 @@ Reading memory trace from mp3d.mem... Read 100000 memory references
 Result: 824 page faults
 */
 
+
 // --- Includes ---
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-// --- Struct for page ---
+
+// Struct so we can keep track of referencing times
 typedef struct Page_struct {
-    int adress;
-    struct Page_struct* next_page;
+    int page_adress;
+    int time_until_next_reference;
 } Page_struct;
 
-// --- Variables for handling the double linked list (page list) ---
-int list_length = 0;
-Page_struct* first_page = NULL;
-Page_struct* last_page = NULL;
 
 // --- Helper functions ---
-
+int prediction_function() {
+    // Implement the prediction function here
+    return 0;
+}
 
 
 // --- Main function ---
 int main(int argc, char *argv[]) {
-    /*
-    Main function for the beladys algorithm program.
-    Calculates the minimum number of page faults for a sequence of memory references.
-    */
 
     // Fail fast if too few arguments
     if (argc < 4) {
-        // Print error message if too few arguments
-        printf("To few args. Use no_phys_pages, page_size, and filename\n");
-        // Return with error
+        printf("Too few args. Use no_phys_pages, page_size, and filename\n");
+        return 1;
+    }
+
+    // Grab the arguments from the command line
+    char* no_phys_pages = argv[1];
+    char* page_size = argv[2];
+    char* filename = argv[3];
+
+    // Convert arguments to int using atoi (ascii to int)
+    int no_phys_pages_int = atoi(no_phys_pages);
+    int page_size_int = atoi(page_size);
+    if (page_size_int <= 0 || no_phys_pages_int <= 0) {
+        printf("Invalid page size or invalid number of physical pages\n");
+        return 1;
+    }
+    // Create array of pages with size no_phys_pages_int
+    // FIXME: This should be an array of Page_structs ?
+
+    // Print the first two arguments as per instructions
+    printf("No physical pages = %d, page size = %d\n", no_phys_pages_int, page_size_int);
+    printf("Reading memory trace from %s... ", filename);
+
+    // Open the file
+    FILE *input_file = fopen(filename, "r");
+    if (input_file == NULL) { // Check for read error
+        printf("Error reading file\n");
         return 1;
     }
 }
