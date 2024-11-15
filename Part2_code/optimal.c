1bf140b7ceb1b1b119fe87bc55baf452f655e138
Simon <muckot@gmail.com>
2024-11-15 16:04:41 +0100

    Task 23 works but needs to be reformatted

--- Part2_code/optimal.c
+++ Part2_code/optimal.c
@@ -21,133 +21,97 @@ Result: 824 page faults
 // --- Includes ---
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
+#define MAX_REFERENCES 100000
 
-// Struct so we can keep track of referencing times
-typedef struct Page_struct {
-    /*
-    Each Page_struct represents a page in memory.
-    Attributes:
-    - page_adress: The address of the page in memory
-    - time_until_next_reference: The number of memory references until the next reference to this page
-    */
-    int page_adress;
-    int time_until_next_reference;
-} Page_struct;
+// Function to find the page to replace based on the future references
+int find_page_to_replace(int *pages, int num_pages, int *references, int ref_count, int current_index) {
+    int farthest = -1;
+    int replace_index = -1;
 
-
-// --- Helper functions ---
-int find_optimal_replace(struct Page_struct **pages, int no_phys_pages) {
-    /*
-    Finds the index of the page that will be referenced the furthest in the future.
-    Arguments:
-    - pages: An array of Page_structs representing the pages in memory
-    - no_phys_pages: The number of physical pages
-    Returns:
-    - The index of the page that will be referenced the furthest in the future
-    */
-
-    // Initialize both vars to NULL so we can check for failure later
-    int max_time = NULL;
-    int max_time_index = NULL;
-
-    // Loop through all pages
-    for (int i = 0; i < no_phys_pages; i++) {
-        // If the page has not been referenced yet or if it will be referenced later than the current max time, update max time
-        if (pages[i]->time_until_next_reference == -1 || pages[i]->time_until_next_reference > max_time) {
-            max_time_index = i;
-            max_time = pages[i]->time_until_next_reference;
+    for (int i = 0; i < num_pages; i++) {
+        int j;
+        for (j = current_index + 1; j < ref_count; j++) {
+            if (pages[i] == references[j]) {
+                if (j > farthest) {
+                    farthest = j;
+                    replace_index = i;
+                }
+                break;
+            }
+        }
+        if (j == ref_count) { // Page not used again
+            return i;
         }
     }
-
-    // Check for failure
-    if (max_time == NULL || max_time_index == NULL) {
-        printf("Error finding optimal replacement\n");
-        return -1; // Return -1 to indicate failure (since both 0 and 1 are valid indexes these are not suitable for error handling)
-    }
-    return max_time_index;
+    return (replace_index != -1) ? replace_index : 0; // Default to first page if all are equally far
 }
 
-void calculate_references(struct Page_struct** pages, struct Page_struct** access_sequence, int no_phys_pages,
-int access_sequence_counter, int* num_pagefaults) {
-    for (int i = 0; i < access_sequence_counter; i++) {
-        bool is_found = false;
-        for (int j = 0; j < no_phys_pages; j++) {
-            if (pages[j]->page_adress == access_sequence[i]->page_adress) {
-                is_found = true;
-                free(pages[j]);
-                pages[j] = access_sequence[i];
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s no_phys_pages page_size filename\n", argv[0]);
+        return 1;
+    }
+
+    int no_phys_pages = atoi(argv[1]);
+    int page_size = atoi(argv[2]);
+    const char *filename = argv[3];
+
+    printf("No physical pages = %d, page size = %d\n", no_phys_pages, page_size);
+    printf("Reading memory trace from %s...", filename);
+
+    // Read memory references from file
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    int references[MAX_REFERENCES];
+    int ref_count = 0;
+    unsigned long address;
+    size_t line_size = 0;
+    char *line;
+
+    while (getline(&line, &line_size, file) != -1) {
+        address = atoi(line);
+        references[ref_count++] = address / page_size; // Convert address to page number
+        if (ref_count >= MAX_REFERENCES) break;
+    }
+    fclose(file);
+    printf("Read %d memory references\n", ref_count);
+
+    // Simulate Optimal Page Replacement
+    int *pages = (int *)malloc(no_phys_pages * sizeof(int));
+    int num_pages = 0, page_faults = 0;
+
+    for (int i = 0; i < ref_count; i++) {
+        int page = references[i];
+        int found = 0;
+
+        // Check if page is already in memory
+        for (int j = 0; j < num_pages; j++) {
+            if (pages[j] == page) {
+                found = 1;
                 break;
             }
         }
 
-        // Case where the page was not found in memory (page fault)
-        if (!is_found) {
-            (*num_pagefaults)++;
-            int replace_index = find_page_to_replace(pages, no_phys_pages);
-            free(pages[replace_index]);
-            pages[replace_index] = access_sequence[i];
-        }
-
-        // Loop through all pages and decrement the time until next reference since we have made a reference
-        for (int j = 0; j < no_phys_pages; j++) {
-            if (pages[j]->time_until_next_reference != -1) {
-                pages[j]->time_until_next_reference--;
+        if (!found) { // Page fault
+            page_faults++;
+            if (num_pages < no_phys_pages) {
+                pages[num_pages++] = page; // Add page if memory is not full
+            } else {
+                // Replace the page using Optimal Replacement Policy
+                int replace_index = find_page_to_replace(pages, num_pages, references, ref_count, i);
+                pages[replace_index] = page;
             }
         }
     }
-}
 
-// --- Main function ---
-int main(int argc, char *argv[]) {
+    free(pages);
 
-    // Fail fast if too few arguments
-    if (argc < 4) {
-        printf("Too few args. Use no_phys_pages, page_size, and filename\n");
-        return 1;
-    }
-
-    // Grab the arguments from the command line
-    char* no_phys_pages = argv[1];
-    char* page_size = argv[2];
-    char* filename = argv[3];
-
-    // Convert arguments to int using atoi (ascii to int)
-    int no_phys_pages_int = atoi(no_phys_pages);
-    int page_size_int = atoi(page_size);
-    if (page_size_int <= 0 || no_phys_pages_int <= 0) {
-        printf("Invalid page size or invalid number of physical pages\n");
-        return 1;
-    }
-
-    // Print the first two arguments as per instructions
-    printf("No physical pages = %d, page size = %d\n", no_phys_pages_int, page_size_int);
-    printf("Reading memory trace from %s... ", filename);
-
-    // Open the file
-    FILE *input_file = fopen(filename, "r");
-    if (input_file == NULL) { // Check for read error
-        printf("Error reading file\n");
-        return 1;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    printf("Result: %d page faults\n", page_faults);
     return 0;
 }
